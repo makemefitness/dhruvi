dc6ab2fbf3135f20e967c929b8c40fb292ba333e
Maciej <msypniewski511@gmail.com>
2023-11-24 03:13:23 +0000

    Add styles from my previous app

--- spec/requests/main_spec.rb
+++ spec/requests/main_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe "Mains", type: :request do
     end
   end
 
+  describe "GET /help" do
+    it "returns http success" do
+      get "/main/help"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /about" do
+    it "returns http success" do
+      get "/main/about"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
